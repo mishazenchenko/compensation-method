81443f53fe0cda5955566aa7b5b2809373e687aa
Michael <mishazenchenkoggg@gmail.com>
2025-02-11 20:24:49 +0300

    amend files

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include "mainwindow.h"
 #include <QApplication>
 
@@ -9,15 +8,3 @@ int main(int argc, char *argv[])
     w.show();
     return a.exec();
 }
-=======
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
-}
->>>>>>> 368ab02 (bash commit)
--- mainwindow.h
+++ mainwindow.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
@@ -40,46 +39,3 @@ protected:
 };
 
 #endif // MAINWINDOW_H
-=======
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
-
-#include <QMainWindow>
-#include <QComboBox>
-#include <QMessageBox>
-#include <QThread>
-#include "compensationmethod.h"
-
-QT_BEGIN_NAMESPACE
-namespace Ui { class MainWindow; }
-QT_END_NAMESPACE
-
-class MainWindow : public QMainWindow
-{
-    Q_OBJECT
-    QThread qth{};
-public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
-    CompensationMethod cm{};
-
-private slots:
-    void instr_lists_activated(QComboBox*);
-    void on_showAuxPanel_clicked();
-    void on_start_clicked();
-    void set_statusBar_text(QString);
-    void set_interface_active();
-    void set_top_value(double);
-    void set_base_value(double);
-    void set_amp_value(double);
-    void show_message(QString);
-    void on_record_clicked();
-
-private:
-    Ui::MainWindow *ui;
-protected:
-    bool eventFilter(QObject *obj,QEvent *event) override;
-};
-
-#endif // MAINWINDOW_H
->>>>>>> 368ab02 (bash commit)
